3038dd8d42884f22d2770c4731d1849479eb78e6
ManoelFranciscoDias <manoel2007dias@gmail.com>
2026-09-18 23:44:48 -0300

    Resolve exercícios 08 e 09 - vetores e intercalação

--- 05-vetores-e-matrizes/exercicio08.py
+++ 05-vetores-e-matrizes/exercicio08.py
@@ -5,3 +5,25 @@
 # VETOR 1: __ __ __ __ __ __ __ __ __ __
 # VETOR 2: __ __ __ __ __ __ __ __ __ __
 # VETOR 3: __ __ __ __ __ __ __ __ __ __
+
+vetor1 = []
+vetor2 = []
+vetor3 = []
+
+print('Digite os 10 elementos do Vetor 1')
+for i in range(10):
+    valor = float(input(f'Digite o valor {i+1}: '))
+    vetor1.append(valor)
+
+print('Digite os 10 elementos do Vetor 2')
+for i in range(10):
+    valor = float(input(f'Digite o valor {i+1}: '))
+    vetor2.append(valor)
+
+for i in range(10):
+    vetor3.append(vetor1[i] + vetor2[i])
+
+print(f'Vetor 1 é: {vetor1}')
+print(f'Vetor 2 é: {vetor2}')
+print(f'A soma dos dois vetores é:')
+print(vetor3)
--- 05-vetores-e-matrizes/exercicio09.py
+++ 05-vetores-e-matrizes/exercicio09.py
@@ -3,3 +3,24 @@
 # b) intercale os elementos desses dois conjuntos formando uma nova variável composta
 #    unidimensional de 50 elementos;
 # c) Escreva o resultado obtido.
+
+variavel_1 = []
+variavel_2 = []
+variavel_3 = []
+
+print('Digite os 25 valores da variavel 1: ')
+for i in range(25):
+    valor = float(input(f'Digite o {i+1} valor: '))
+    variavel_1.append(valor)
+
+print('Digite os 25 valores da variavel 2: ')
+for i in range(25):
+    valor = float(input(f'Digite o {i+1} valor: '))
+    variavel_2.append(valor)
+
+for i in range(25):
+    variavel_3.append(variavel_1[i])
+    variavel_3.append(variavel_2[i])
+
+print('Intercalando as duas variaveis temos:')
+print(variavel_3)
