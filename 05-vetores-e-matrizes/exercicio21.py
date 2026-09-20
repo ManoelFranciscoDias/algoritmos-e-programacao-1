629398f5145e66a9ce9e185789f56c4e844da2f3
ManoelFranciscoDias <manoel2007dias@gmail.com>
2026-09-20 10:26:36 -0300

    resolve exericios 21 de vetores e matrizes

--- 05-vetores-e-matrizes/exercicio21.py
+++ 05-vetores-e-matrizes/exercicio21.py
@@ -1,2 +1,20 @@
 # Exercício 21: Elabore um algoritmo que leia uma matriz 4 x 4 e escreva a matriz resultante
 # após ter multiplicado os elementos da diagonal principal por uma constante k.
+
+matriz = []
+for i in range(4):
+    linha = []
+    for j in range(4):
+        linha.append(int(input(f'Digite o elemento [{i}][{j}] da matriz: ')))
+    matriz.append(linha)
+
+k = int(input('Informe qual é a constante k: '))
+
+for i in range(4):
+    matriz[i][i] *= k
+
+print('Conteudo da Matriz')
+for i in range(4):
+    for j in range(4):
+        print(f'{matriz[i][j]:4}', end="")
+    print()
