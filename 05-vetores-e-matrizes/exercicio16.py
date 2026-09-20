b6f1b7b8d7e5cece9de9e03dca8e0855218bcaad
ManoelFranciscoDias <manoel2007dias@gmail.com>
2026-09-19 22:14:52 -0300

    Resolve exercicios 16 de vetores e matrizes

--- 05-vetores-e-matrizes/exercicio16.py
+++ 05-vetores-e-matrizes/exercicio16.py
@@ -2,3 +2,21 @@
 # no vetor. Se existir, imprimir a posição onde foi encontrada a chave; se não, imprimir a
 # mensagem: "CHAVE K NÃO ENCONTRADA". O vetor A e a chave K são lidos a partir de uma unidade de
 # entrada.
+
+K = int(input('Digite a chave K: '))
+
+A = []
+posicoes = []
+
+
+for i in range(128):
+    elemento = int(input(f'Digite o elemento {i+1}: '))
+    A.append(elemento)
+
+    if elemento == K:
+        posicoes.append(i + 1)
+
+if len(posicoes) == 0:
+    print("CHAVE K NÃO ENCONTRADA")
+else:
+    print(f'Chave {K} encontrada na(s) posição(ões): {posicoes}')
